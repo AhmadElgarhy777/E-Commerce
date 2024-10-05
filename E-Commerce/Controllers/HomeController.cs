7076ce14b5ac8e9ec0c3e935c08e54b6aa4b8257
Ahmad El Gar7y <ahmadelgary@gmail.com>
2024-10-06 01:16:23 +0300

    first commit

---
+++ E-Commerce/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace E_Commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            AppDbContext dbContext = new AppDbContext();
+            var item=dbContext.Products.ToList();
+            return View(model: item);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ E-Commerce/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=E-Commerce; Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
